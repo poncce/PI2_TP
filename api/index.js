cc45555c9da03e77e2207338ebfc08b8101c5f24
poncce <ponce.ignacioet36@gmail.com>
2025-10-22 01:40:36 +0200

    Se ordena el codigo agregando comentarios para facilitar su entendimiento

--- api/index.js
+++ api/index.js
@@ -1,48 +1,60 @@
-// index
+
 const express = require('express');
-const { sayHello } = require('./controller/sayHello');
-const { getActiveUsers, registerUser, login, me, createAdmin, getActiveUserProfile,} = require('./controller/user');
-const { isAuth, isAdmin } = require('./middlewares/auth');
-const { canRatePost } = require('./middlewares/canRatePost');
-const { checkUserStatus } = require('./middlewares/checkUserStatus');
-const { isOwnerOfComment } = require('./middlewares/isOwnerOfComment');
-const { isOwnerOfPost } = require('./middlewares/isOwnerOfPost');
-
-
 const { sequelize } = require('./config/db');
-const { createPost } = require('./controller/post');
-const server = express();
 
-server.use(express.json())
+// Controllers
+const { 
+  getActiveUsers,
+  registerUser,
+  login,
+  me,
+  createAdmin,
+  getActiveUserProfile,
+} = require('./controller/user');
+const { createPost } = require('./controller/post');
+
+// Middlewares
+const { isAuth, isAdmin } = require('./middlewares/auth');
+const { checkUserStatus } = require('./middlewares/checkUserStatus');
+const { agregarComentario } = require('./controller/comment');
+
+const server = express();
+server.use(express.json());
+
+// CORS para vite
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
 });
 
 
 
-server.get('/', sayHello)
-server.get('/users', isAuth, getActiveUsers)
-server.get('/me', isAuth, me)
-server.post('/users', registerUser)
-server.post('/login', login)
-server.post('/admin/create', createAdmin)
-server.get('/users/profile', isAuth, getActiveUserProfile)
-server.post('/posts', isAuth, checkUserStatus, createPost)
+// Usuarios / Auth
+server.get('/users', isAuth, getActiveUsers);
+server.get('/me', isAuth, me);
+server.get('/users/profile', isAuth, getActiveUserProfile);
+
+server.post('/users', registerUser);
+server.post('/login', login);
+server.post('/admin/create', createAdmin);
+
+
+// Posts
+
+server.post('/posts', isAuth, checkUserStatus, createPost);
+
+
+// Comentarios
+server.post('/posts/:postId/comments', isAuth, checkUserStatus, agregarComentario)
 
 
 
-
+// Arrancar server
 server.listen(3000, async () => {
-    await sequelize.sync({ force: false })
-    console.log('El server se esta ejecutando en el puerto 3000');
-
-})
-
-
+  await sequelize.sync({ force: false }); 
+  console.log('El server se está ejecutando en el puerto 3000');
+});
