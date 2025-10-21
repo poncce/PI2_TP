b613dc6cd1eb23b62df6bd7263c6b7612dc2f121
poncce <ponce.ignacioet36@gmail.com>
2025-10-22 01:38:27 +0200

    Cambio de parametros que recibe la funcion agregarComentario

--- api/controller/comment.js
+++ api/controller/comment.js
@@ -1,11 +1,21 @@
-const { Comment } = require("../models/Comments");
-const { Post } = require("../models/Posts");
+const { Comment } = require("../models/Comment");
+const { Post } = require("../models/Post");
 
-async function agregarComentario({ postId, autorId, contenido }) {
+async function agregarComentario(req, res) {
+  const { postId } = req.params;    
+  const autorId = req.user.id;       
+  const { contenido } = req.body;   
+
+
+  if (!contenido) {
+    return res.status(400).json({ message: "Falta el contenido del comentario" });
+  }
+
   const post = await Post.findByPk(postId);
   if (!post) {
-    res.json({ message: "Post no encontrado" });
-}
+    return res.status(404).json({ message: "Post no encontrado" });
+  }
+
   const comentario = await Comment.create({
     PostId: postId,
     autorId,
@@ -13,9 +23,11 @@ async function agregarComentario({ postId, autorId, contenido }) {
     fechaComentario: new Date(),
   });
 
-  res.json({ message: "Comentario agregado exitosamente" });
+  res.status(201).json({
+    message: "Comentario agregado exitosamente"
+  });
 }
 
 module.exports = {
-  agregarComentario,
-};
+  agregarComentario
+}
