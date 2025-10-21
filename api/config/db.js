88621c4745512b2982b1bf0611d8764066a24a62
poncce <ponce.ignacioet36@gmail.com>
2025-10-22 01:37:38 +0200

    Cambios en la configuracion de la database

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('proyecto', 'root', '', {
+const sequelize = new Sequelize('paulinacultiva', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false
