7e84af1826e38a4a738cf5992d4599f323bb7ff0
poncce <ponce.ignacioet36@gmail.com>
2025-11-02 19:08:26 +0100

    volver a crear modelo Calification.js, tampoco se pq se borro

--- api/models/Calification.js
+++ api/models/Calification.js
@@ -1,26 +1,28 @@
-module.exports = (sequelize, DataTypes) => {
-  const Calification = sequelize.define('Calificacion', {
+const { sequelize } = require("../config/db");
+const { DataTypes } = require('sequelize');
+
+const Calification = sequelize.define('Calificacion', {
     userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+        type: DataTypes.INTEGER,
+        allowNull: false,
     },
     postId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
+        type: DataTypes.INTEGER,
+        allowNull: false,
     },
     score: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: { min: 1, max: 5 }
-    },
-    dvh: {
-      type: DataTypes.INTEGER,
-      allowNull: true
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1,
+            max: 5
+        }
     }
-  }, {
+}, {
     timestamps: false,
     modelName: 'Calificacion'
-  });
+});
 
-  return Calification;
-};
+    module.exports = {
+        Calification
+    };
